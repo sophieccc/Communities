d31bad5a56f3ad22d14085a22bf3712b954ae468
Sophie Crowley <crowleso@tcd.ie>
2022-03-16 12:29:13 +0000

    Data in json

---
+++ get_reddit_data.py
@@ -0,0 +1,38 @@
+import json
+import requests
+
+subreddit = 'techsupport'
+limit = 1000
+listing = 'new'  # controversial, best, hot, new, random, rising, top
+
+
+def get_reddit(subreddit, listing, limit, after=''):
+    r_all = []
+    while len(r_all) < 1000:
+        try:
+            base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}{after}'
+            request = requests.get(base_url, headers={'User-agent': 'yourbot'})
+            r = request.json()
+            after = "&after=" + r["data"]["after"]
+            r_all += r["data"]["children"]
+        except:
+            print('An Error Occured')
+            return r_all
+    return r_all
+
+
+r_all = get_reddit(subreddit, listing, limit)
+keys = ["selftext", "author", "title", "id", "created_utc", "num_comments"]
+with open('techsupport.json', 'a') as f:
+    json_objs = []
+    for post in r_all:
+        json_obj = {
+            "id": post["data"]["id"],
+            "created": post["data"]["created_utc"],
+            "num_comments": post["data"]["num_comments"],
+            "text": post["data"]["selftext"],
+            "authot": post["data"]["author"],
+            "title": post["data"]["title"]
+        }
+        json_objs.append(json_obj)
+    json.dump(json_objs, f)
